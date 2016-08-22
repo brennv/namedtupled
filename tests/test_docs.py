3c134dcae2569bd97cd009389055b721cca8a81c
brennv <brennv@users.noreply.github.com>
2016-08-22 08:12:33 -0700

    add test_env

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -45,8 +45,11 @@ def test_yaml():
 
 
 def test_env():
+    # In shell first:
     # export USERNAME="binks"
     # export APIKEY="c4tnip!"
+    # Or run:
+    # source ./tests/setenv.sh
     variables = ['USERNAME', 'APIKEY']
     cat = namedtupled.env(variables)
     assert cat.USERNAME == 'binks'
